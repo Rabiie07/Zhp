bfdb68744040dc23b068528e17ecacdff8917162
rabie <rabie@ELBEHJA>
2019-01-01 15:12:47 +0100

    float

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //this is my first app
             Console.WriteLine("Hello World");
-            Console.ReadKey();
+
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+
+            Console.WriteLine("Minvalue= {0} Maxvalue= {1}", flMin, flMax);
+
+            float flVar = 1F / 3;
+                double dlVar = 1D/ 3;
+                decimal dcVar =  1M / 3;
+            Console.WriteLine("\nfloat value ={0} \ndouble value ={1} \ndecimal value = {2}" , flVar , dlVar , dcVar );
+               
+                Console.ReadKey();
+            
             
 
 
